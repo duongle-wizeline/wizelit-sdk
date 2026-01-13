421538df8479a15f9952d7df58703d961f6548a3
Duong Le <duong.le@wizeline.com>
2026-01-13 12:23:42 +0700

    update __init__ file

--- src/agent_wrapper/__init__.py
+++ src/agent_wrapper/__init__.py
@@ -1,6 +1,6 @@
 """Agent Wrapper - Internal utility package."""
 
 # Import main functions
-from .utils import greet, greet_many
+from .utils import greet, greet_many, greet_many3
 
-__all__ = ["greet", "greet_many"]
+__all__ = ["greet", "greet_many", "greet_many3"]
